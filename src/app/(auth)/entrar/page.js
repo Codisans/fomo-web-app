626cabbaa94b91a2743ac5b61f3468cc0465378d
Nicolascanala <nicolas.canala@gmail.com>
2024-08-29 14:57:18 +0200

    wip some fixes

--- src/app/(auth)/entrar/page.js
+++ src/app/(auth)/entrar/page.js
@@ -56,7 +56,7 @@ const Login = () => {
                         id="email"
                         type="email"
                         value={email}
-                        className="block mt-1 w-full"
+                        className="block w-full mt-1"
                         onChange={event => setEmail(event.target.value)}
                         required
                         autoFocus
@@ -73,7 +73,7 @@ const Login = () => {
                         id="password"
                         type="password"
                         value={password}
-                        className="block mt-1 w-full"
+                        className="block w-full mt-1"
                         onChange={event => setPassword(event.target.value)}
                         required
                         autoComplete="current-password"
@@ -91,7 +91,7 @@ const Login = () => {
                             id="remember_me"
                             type="checkbox"
                             name="remember"
-                            className="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            className="text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             onChange={event =>
                                 setShouldRemember(event.target.checked)
                             }
@@ -104,7 +104,7 @@ const Login = () => {
                 </div>
 
                 <div className="flex flex-col items-center gap-4 mt-4">
-                    <Button>Entrar</Button>
+                    <Button type="submit">Entrar</Button>
                     <TextLink href="/recuperar-clave">Recuperar clave</TextLink>
                     <Button href="/crear-cuenta">Crear cuenta</Button>
                 </div>
